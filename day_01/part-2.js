322277ffee90f6fd495850401048dabd16436733
Gideon Spierings <gideongideon@gmail.com>
2018-12-07 11:22:20 +0100

    Refactored day 1 so I can benchmark it

--- day_01/part-2.js
+++ day_01/part-2.js
@@ -1,14 +1,20 @@
 const input = require('./input');
+const { benchmark } = require('../utils');
 
 const getValue = (collection, index) => collection[index % collection.length];
 
-const map = {};
-let current = 0;
+const getAnswer = () => {
+  const map = {};
+  let current = 0;
 
-for (let i = 0; i < Infinity; i++) {
-  current += getValue(input, i);
-  if (map[current]) {
-    return console.log('Answer: ', current);
+  for (let i = 0; i < Infinity; i++) {
+    current += getValue(input, i);
+    if (map[current]) {
+      return current;
+    }
+    map[current] = current;
   }
-  map[current] = current;
-}
+};
+
+console.log('Answer: ', getAnswer());
+console.log('Benchmark: ', benchmark(getAnswer, 1000));
